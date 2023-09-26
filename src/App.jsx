2c44bdee3fa7bff767179b680269c551affcfcbb
faladetimilehin <faladetimilehin@yahoo.com>
2023-09-26 20:53:22 +0100

    Initial commit for matching game

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Game from './components/Game'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Memory Game</h1>
+      <Game />
+    </>
+  )
+}
+
+export default App
