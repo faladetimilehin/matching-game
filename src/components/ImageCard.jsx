bc880eb442e638ff02daf0859d22d650a737c7e6
ADE OLAOYE <adeolaoye56@gmail.com>
2023-09-29 16:09:57 +0100

    add flipping feature to image gallery/answers

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+// import React from 'react'
 
-const ImageCard = ({src, title}) => {
+// const ImageCard = ({src, title}) => {
+//   return (
+//     <img 
+//     src={src} 
+//     alt={title}
+//     style={{height: 150, width: 150, borderRadius: 6}}
+//     />
+//   )
+// }
+
+// export default ImageCard
+
+import React, {useState} from 'react'
+
+const ImageCard = ({frontImageSrc, backContent}) => {
+const[isFlipped, setIsFlipped] = useState(false)
+
+const handleCardFlip = () => {
+  setIsFlipped(!isFlipped)
+}
+
   return (
-    <img 
-    src={src} 
-    alt={title}
-    style={{height: 150, width: 150, borderRadius: 6}}
-    />
+    <div className={`card ${isFlipped ? 'flipped': ''}`} onClick={handleCardFlip}>
+      <div className='card-front'>
+        <img 
+          src={frontImageSrc} 
+        alt='front-side'
+        style={{height: 150, width: 150, borderRadius: 6}}
+        />
+      </div>
+      <div className='card-back'>
+        {backContent}
+      </div>
+    </div>
   )
 }
 
--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -1,16 +1,51 @@
+// import React from 'react'
+// import ImageCard from './ImageCard'
+// import images from './imagesData.json'
+
+// const ImageGrid = () => {
+//     return (
+//         <div className='image-grid-wrapper'>
+//         {images.map((image) => {
+//             return (
+//                 <div className='gallery-wrapper'><ImageCard key={image.id} {...image} />
+//                 </div>
+//             )
+//         })}
+//         </div>
+//     )
+// }
+
+// export default ImageGrid
+
 import React from 'react'
 import ImageCard from './ImageCard'
-import images from './imagesData.json'
+
+const imagesData = [
+    {
+      "frontImageSrc": "/images/angry1.jpg",
+      "backContent": "ANGRY"
+    },
+    {
+      "frontImageSrc": "/images/excited1.jpg",
+      "backContent": "EXCITED"
+    },
+    {
+      "frontImageSrc": "/images/excited2.jpg",
+      "backContent": "EXCITED"
+    },
+    {
+      "frontImageSrc": "/images/angry2.jpg",
+      "backContent": "ANGRY"
+    }
+  ]
 
 const ImageGrid = () => {
     return (
-        <div className='image-grid-wrapper'>
-        {images.map((image) => {
-            return (
-                <div className='gallery-wrapper'><ImageCard key={image.id} {...image} />
-                </div>
+        <div className='gallery-wrapper'>
+        {imagesData.map((image, index) =>  (
+               <ImageCard key={index} frontImageSrc={image.frontImageSrc} backContent={image.backContent} />
             )
-        })}
+        )}
         </div>
     )
 }
