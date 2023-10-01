15cca56c2cfbb2740c4e918c81440c93401f5574
faladetimilehin <faladetimilehin@yahoo.com>
2023-10-01 21:53:27 +0100

    matching game

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import background from '/images/background.jpg'
 
-const LandingPage = ({handleStart}) => {
+const LandingPage = ({ handleStart }) => {
     return (
         <section className='home-wrapper'>
             <div className='home-caption'>
                 <h1>Matching Game</h1>
-                <h2>A fun and interactive facial expressions memory game aimed at children with autism, ADHD and communication difficulties.</h2>
+                <h2>A fun and interactive facial expressions memory game aimed for individuals.</h2>
                 <button onClick={handleStart}>Start!</button>
             </div>
             <img src={background} alt='photo' className='homepage-img' />
